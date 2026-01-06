f1bc2c36ba04cf0cdb889e73f37a360aef792282
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2026-01-06 23:46:39 +0100

    gitignore dist folder

--- dist/models/blockFee.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const BlockFeeSchema = new mongoose.Schema({
-    block_height: { type: Number, unique: true, index: true },
-    block_timestamp: { type: Number, index: true },
-    total_fees_stx: Number
-}, { timestamps: true });
-const collectionName = process.env.Fees_COLLECTION || "stacks-fees";
-export const BlockFee = mongoose.models[collectionName] ||
-    mongoose.model("BlockFee", BlockFeeSchema, collectionName);
